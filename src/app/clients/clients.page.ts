77fa68dfb7c212c0d0a80f7ac36e8b764fa13ede
David Carrera <142541347+david-carrerx@users.noreply.github.com>
2025-05-07 14:56:24 -0600

    Filtro de busqueda completado

--- src/app/clients/clients.page.ts
+++ src/app/clients/clients.page.ts
@@ -14,6 +14,10 @@ import { Router } from '@angular/router'
 })
 export class ClientsPage implements OnInit {
   clients: any[] = [];
+  filteredClients: any[] = [];
+  searchTerm: string = '';
+  selectedFilter: string = 'todos';
+
 
   constructor(private http: HttpClient) { }
 
@@ -22,9 +26,47 @@ export class ClientsPage implements OnInit {
   ngOnInit() {
     this.http.get<any[]>('http://localhost:3000/api/clients/clients')
       .subscribe({
-        next: (data) => this.clients = data,
-        error: (e) => console.log(e)
+        next: (data) => {
+          this.clients = data;
+          this.filteredClients = data;
+        },
+        error: (err) => console.error(err)
       });
+  }
+
+  filterClients() {
+    const term = this.searchTerm.toLowerCase();
+  
+    this.filteredClients = this.clients.filter(client => {
+      const matchesName = client.name.toLowerCase().includes(term);
+  
+      if (this.selectedFilter === 'activo') {
+        return matchesName && this.isClientActive(client.end_date) && !this.isExpiringSoon(client.end_date);
+      } else if (this.selectedFilter === 'inactivo') {
+        return matchesName && !this.isClientActive(client.end_date);
+      } else if (this.selectedFilter === 'pronto') {
+        return matchesName && this.isExpiringSoon(client.end_date);
+      } else if (this.selectedFilter === 'mes') {
+        const end = new Date(client.end_date);
+        const today = new Date();
+        return (
+          matchesName &&
+          end.getMonth() === today.getMonth() &&
+          end.getFullYear() === today.getFullYear()
+        );
+      } else {
+        return matchesName; // todos
+      }
+    });
+  }
+  
+
+  isExpiringSoon(endDate: string | Date): boolean {
+    const today = new Date();
+    const end = new Date(endDate);
+    const diffInMs = end.getTime() - today.getTime();
+    const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+    return diffInDays > 0 && diffInDays <= 3;
   }
 
   isClientActive(endDate: string | Date): boolean {
